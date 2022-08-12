04a4b06d91a4e516aa03e8c4a73f76f31681c66a
Mauch <sven@mauch.me>
2022-08-12 20:14:24 +0200

    remove unused resource

--- WinSlap/Resources/delapps1.ps1
+++
@@ -1,2 +0,0 @@
-    Set-ExecutionPolicy Unrestricted
-    Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsStore*"} | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.name -notlike "*Microsoft.Windows.Photos*"} | Remove-AppxPackage
