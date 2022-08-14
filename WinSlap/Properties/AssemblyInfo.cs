7b381d530821c5be1bb1d7c778d43753de75c97b
Sven Mauch <sven@mauch.me>
2022-08-14 17:12:00 +0200

    change copyright year to 2022

--- WinSlap/Properties/AssemblyInfo.cs
+++ WinSlap/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinSlap")]
-[assembly: AssemblyCopyright("Copyright © Sven Mauch 2021")]
+[assembly: AssemblyCopyright("Copyright © Sven Mauch 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
